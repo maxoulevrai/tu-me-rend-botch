7ed198a4ef73d76c07606cc360e4d58a63f2950e
root <root@DESKTOP-AQPOE75>
2024-09-19 15:39:30 +0200

    enfin

--- piscine/C03/ex04/ft_strstr.c
+++ piscine/C03/ex04/ft_strstr.c
@@ -3,16 +3,29 @@
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
-	int	c;
+	int	tmp;
 
 	i = 0;
-	c = 0;
+	tmp = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] != to_find[c])
-			i++;
-		else if (str[i] == to_find[c])
-			while (str[i] == to_find[c])
+		while (str[i + tmp] == to_find[tmp])
+		{
+			if (to_find[tmp + 1] == '\0')
+				return (&str[i]);
+			tmp++;
+		}
+		tmp = 0;
+		i++;
+	}
+	return (0);
+}
 
-	}	
+int	main(void)
+{
+	char	s1[] = "bonjour le monde";
+	char	s2[] = "le";
+
+	printf("%s\n", ft_strstr(s1, s2));
+	return(0);
 }
