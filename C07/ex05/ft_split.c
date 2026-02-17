2fe90bbff53e9aa563bb2975460bfce775b5bc86
root <root@LAPTOP-6I81QJ0S.localdomain>
2026-02-17 17:49:37 +0100

    split

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -1,112 +1,129 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maleca <maleca@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 18:09:17 by maleca            #+#    #+#             */
+/*   Updated: 2025/02/04 20:15:29 by maleca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <stdio.h>
 
-int	is_charset(char *str, char *charset)
+int	is_charset(char c, char *charset)
 {
-	int		i;
+	int	i;
 
 	i = 0;
-	while (charset[i] != '\0')
+	while (charset[i])
 	{
-		if (str[i] != charset[i])
-			return (0);
+		if (charset[i] == c)
+			return (1);
 		i++;
 	}
-	return (1);
+	return (0);
 }
 
-int	word_len(char *str, char *charset)
+int	get_word_len(char *str, char *charset)
 {
-	int		i;
+	int	i;
 
 	i = 0;
-	while (str[i] != '\0' && is_charset(&str[i], charset) == 0)
+	while (str[i] && is_charset(str[i], charset) == 0)
 		i++;
 	return (i);
 }
 
-int	tab_len(char *str, char *charset)
+int	get_tab_len(char *str, char *charset)
 {
-	int		count;
-	int		i;
-	int		len;
+	int	i;
+	int	count;
+	int	len;
 
-	i = 0;
 	count = 0;
-	while (str[i] != '\0')
+	i = 0;
+	while (str[i])
 	{
-		while (str[i] != '\0' && is_charset(&str[i], charset) == 0)
+		while (str[i] && is_charset(str[i], charset) == 1)
 			i++;
-		if (str[i] != '\0')
-		{	
+		if (str[i])
+		{
 			count++;
-			len = word_len(&str[i], charset);
-			i = i + len;
+			len = get_word_len(&str[i], charset);
+			i += len;
 			i++;
 		}
 	}
 	return (count);
 }
 
-char	*ft_strdup(char *src, int len)
+char	*ft_strndup(char *src, int len)
 {
-	char	*dest;
 	int		i;
+	char	*dst;
 
+	if (src == NULL || len < 0)
+		return (NULL);
+	dst = malloc(sizeof(char) * len + 1);
+	if (dst == NULL)
+		return (NULL);
 	i = 0;
-	if (src == NULL)
-		return (NULL);
-	dest = malloc(sizeof(char) * len + 1);
-	if (dest == NULL)
-		return (NULL);
-	while(i < len)
+	dst[i] = '\0';
+	while (i < len)
 	{
-		dest[i] = src[i];
+		dst[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	dst[i] = '\0';
+	return (dst);
 }
 
 char	**ft_split(char *str, char *charset)
 {
+	char	**strs;
 	int		i;
 	int		j;
-	char	**strs;
 
-	strs = malloc(sizeof(char*) * tab_len(str, charset) + 1);
+	if (str == NULL || charset == NULL)
+		return (NULL);
+	strs = malloc(sizeof(char *) * get_tab_len(str, charset) + 1);
 	if (strs == NULL)
 		return (NULL);
+	strs[0] = NULL;
 	i = 0;
 	j = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		while (str[i] != '\0' && is_charset(&str[i], charset))
+		while (str[i] && is_charset(str[i], charset) == 1)
 			i++;
-		if (str[i] != '\0')
+		if (str[i])
 		{
-			strs[j] = ft_strdup(&str[i], word_len(&str[i], charset));
+			strs[j] = ft_strndup(&str[i], get_word_len(&str[i], charset));
 			j++;
-			i = i + word_len(&str[i], charset);
+			i += get_word_len(&str[i], charset);
 		}
 	}
-	strs[j] = NULL;
 	return (strs);
 }
-
-int	main(int argc, char **argv)
+/*
+int	main(void)
 {
-	(void)argc;
-	int	i;
-	char	**strs;
+	int		i;
+	char **strs = NULL;
+	char *str = "bonjour/les/amis";
 
+	strs = ft_split(str, "/");
 	i = 0;
-	strs = ft_split(argv[1], argv[2]);
-	while (strs[i] != NULL)
-	{	
+	while (strs[i])
+	{
 		printf("%s\n", strs[i]);
+		free(strs[i]);
 		i++;
 	}
 	free(strs);
 	return (0);
 }
+*/
